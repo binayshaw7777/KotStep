3f0a953da8c4bd6867a45d2fe9f660b7c969ac2d
Binay Shaw <binayshaw7777@gmail.com>
2024-08-20 10:43:50 +0530

    Moved 2.1.0 to 2.2.0

--- kotstep/build.gradle.kts
+++ kotstep/build.gradle.kts
@@ -62,7 +62,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.binayshaw7777"
             artifactId = "KotStep"
-            version = "2.1.0"
+            version = "2.2.0"
 
             afterEvaluate {
                 from(components["release"])
