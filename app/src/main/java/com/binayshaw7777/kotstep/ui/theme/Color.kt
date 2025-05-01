5a227013b6179e5e8233577d25f9e55e690e3317
Binay Shaw <binayshaw7777@gmail.com>
2025-05-01 20:55:35 +0530

    feat: Add Groww App like KotStep example

--- app/src/main/java/com/binayshaw7777/kotstep/ui/theme/Color.kt
+++ app/src/main/java/com/binayshaw7777/kotstep/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val GROWW_BACKGROUND = Color(0xFF121212)
+val GROWW_GREEN = Color(0xFF00D09C)
+
+
+val AMAZON_GREEN = Color(0xFF4DC1B4)
+val AMAZON_TEXT_PRIMARY_GREEN = Color(0xFF053134)
+val AMAZON_TEXT_SECONDARY_GREEN = Color(0xFF11818C)
--- app/src/main/java/com/binayshaw7777/kotstep/ui/theme/presentation/v2/KotStepPreview.kt
+++ app/src/main/java/com/binayshaw7777/kotstep/ui/theme/presentation/v2/KotStepPreview.kt
@@ -3,7 +3,9 @@ package com.binayshaw7777.kotstep.ui.theme.presentation.v2
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,21 +13,86 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.binayshaw7777.kotstep.ui.theme.presentation.v2.steppers.GrowwPreview
+import com.binayshaw7777.kotstep.utils.StepperOptions
 import com.binayshaw7777.kotstep.utils.Utils.getKotStepStyle
 import com.binayshaw7777.kotstep.v3.model.step.StepLayoutStyle
 import com.binayshaw7777.kotstep.v3.samples.KotStepHorizontalExample
 import com.binayshaw7777.kotstep.v3.samples.KotStepVerticalExample
 import com.binayshaw7777.kotstep.v3.util.ExperimentalKotStep
+
+enum class KotStepExampleTypes {
+    GROWW_APP,
+    AMAZON_APP
+}
+
+@Composable
+fun KotStepExamples() {
+
+    var expanded by remember { mutableStateOf(false) }
+    var exampleType by remember { mutableStateOf(KotStepExampleTypes.GROWW_APP) }
+
+
+    Column(modifier = Modifier.fillMaxSize().background(Color.DarkGray)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box {
+                IconButton(onClick = { expanded = true }) {
+                    Icon(Icons.Default.MoreVert, contentDescription = "Localized description")
+                }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text("Groww App") },
+                        onClick = {
+                            exampleType = KotStepExampleTypes.GROWW_APP
+                            expanded = false
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Amazon Appp") },
+                        onClick = {
+                            exampleType = KotStepExampleTypes.AMAZON_APP
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+        when (exampleType) {
+            KotStepExampleTypes.GROWW_APP -> {
+                GrowwPreview()
+            }
+            KotStepExampleTypes.AMAZON_APP -> {
+
+            }
+        }
+    }
+}
 
 @OptIn(ExperimentalKotStep::class)
 @Preview(showBackground = true, backgroundColor = 0xFF1C2526)
