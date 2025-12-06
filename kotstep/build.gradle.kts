dcb9c0a9548b664e2dbb14f4bb416ee6466f8763
Binay Shaw <binayshaw7777@gmail.com>
2025-12-07 02:52:32 +0530

    feat: Bump version to 3.1.0 in build configuration

--- kotstep/build.gradle.kts
+++ kotstep/build.gradle.kts
@@ -69,7 +69,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.binayshaw7777"
             artifactId = "KotStep"
-            version = "3.0.1"
+            version = "3.1.0"
 
             afterEvaluate {
                 from(components["release"])
